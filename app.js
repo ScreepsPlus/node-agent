d9ea75a1067ba9f5dcf918c959ba878033088ed7
Adam Shumann <ashumann13@gmail.com>
2017-03-28 09:07:39 -0500

    Slight stats rework, updated to now include new formats

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const path = require('path');
 const updateNotifier = require('update-notifier');
 const ScreepsAPI = require('screeps-api')
 const request = require('request')
@@ -25,6 +26,7 @@ function start(){
     updateNotifier({pkg}).notify();
   api.auth(config.screeps.username,config.screeps.password,(res)=>{
     console.log(res,'Authenticated')
+    console.log('Using stats method',config.screeps.method)
     if(config.screeps.method == 'console')
       beginConsoleStats()
     else
@@ -45,10 +47,7 @@ function beginConsoleStats(){
     if(data.messages && data.messages.log)
       data.messages.log
         .filter(l=>l.startsWith('STATS'))
-        .forEach(log=>{
-          let ret = formatStats(log)
-          pushStats(ret.type,ret.stats)
-        })
+        .forEach(log=>processStats(log))
   })
 }
 
@@ -62,26 +61,66 @@ function formatStats(data){
   if(type.startsWith('text')){
     stats = stats.map(s=>`${s} ${time}`).join("\n")
   }
-  return { header,type,tick,time,stats }
+  return Promise.resolve({ header,type,tick,time,stats })
 }
 
 function beginMemoryStats(){
   tick()
   setInterval(tick,15000)
 }
+function addProfileData(stats){
+    return api.me().then(res=>{
+      let credits = res.body.credits || 0
+      let power = res.body.power || 0
+      if(stats.type == 'application/json'){
+        stats.stats.credits = credits
+        stats.stats.power = power
+      }
+      if(stats.type == 'text/grafana'){
+        stats.stats += `credits ${credits} ${Date.now()}\n`
+        stats.stats += `power ${power} ${Date.now()}\n`
+      }
+      if(stats.type == 'text/influxdb')
+        stats.stats += `profile,user=${api.user.username} credits=${credits},power=${power} ${Date.now()}\n`
+      return stats
+    });
+}
+function addLeaderboardData(stats){
+    let leaderboardUrl = `/api/leaderboard/find?mode=world&username=${api.user.username}`;
+    return api.req('GET', leaderboardUrl).then(res=>{
+      let { rank, score } = res.body.list.slice(-1)[0];
+      if(stats.type == 'application/json'){
+        stats.stats.leaderboard.rank = rank
+        stats.stats.leaderboard.score = score
+      }
+      if(stats.type == 'text/grafana'){
+        stats.stats += `leaderboard.rank ${rank} ${Date.now()}\n`
+        stats.stats += `leaderboard.score ${score} ${Date.now()}\n`
+      }
+      if(stats.type == 'text/influxdb')
+        stats.stats += `leaderboard,user=${api.user.username} rank=${rank},score=${score} ${Date.now()}\n`
+      return stats
+    });
+}
 
 function tick(){
   Promise.resolve()
     .then(()=>console.log('Fetching Stats'))
     .then(()=>api.memory.get('stats'))
-    .then((stats)=>{
-      let ret = formatStats(stats)
-      pushStats(ret.type,ret.stats)
-    })
+    .then(processStats)
     .catch(err=>console.error(err))
 }
 
-function pushStats(type,stats){
+function processStats(data){
+  return Promise.resolve(data)
+    .then(formatStats)
+    .then(addLeaderboardData)
+    .then(addProfileData)
+    .then(pushStats)
+}
+
+function pushStats(data){
+  let {type,stats} = data
   if(!stats) return console.log('No stats found, is Memory.stats defined?')
   if(config.showRawStats) console.log('Stats:',JSON.stringify(stats,null,3))
   console.log('Pushing stats')
@@ -99,10 +138,10 @@ function pushStats(type,stats){
     },
     body: stats
   },(err,res,data)=>{
-    if(res.statusCode == 413){
+    if(res && res.statusCode == 413){
       let len = Math.round(JSON.stringify(stats).length/1024)
       console.log(`stats size: ${len}kb`)
-      console.log(`stats limit: 20kb (As of Oct 17, 2016)`)
+      console.log(`stats limit: 10mb (As of Mar 28, 2017) (If you hit this limit, you are probably doing something wrong)`)
       console.error(`It appears your stats data is too large, please check to make sure you are not submitting unneeded stats, such as old rooms. \n If you legitimately need to submit stats this large, contact ags131 on slack for a limit bump`)
     }
     console.log('Result:',data)
@@ -124,23 +163,34 @@ function setup(){
     })
   }else{
     console.log('Please setup config.js before running.')
+    console.log(`Valid paths for your platform (${process.platform}):`)
+    getConfigPaths().paths.forEach(path=>console.log(`- ${path}`))
+    console.log()
+    console.log('Or set the AGENT_CONFIG_PATH environment variable to point to a valid config file.')
   }
 }
 
 function getConfigPaths(){
-  let paths = [
-    './config'
-  ]
+  let appname = 'screepsplus-agent'
+  let paths = []
+  if(process.env.AGENT_CONFIG_PATH)
+    paths.push(process.env.AGENT_CONFIG_PATH)
+  paths.push(path.join(__dirname,'config.js'))
   let create = ''
   if(process.platform == 'linux'){
-    create = `${process.env.HOME}/.screepsplus-agent`
+    create = `${process.env.HOME}/.${appname}`
     paths.push(create)
-    paths.push(`/etc/screepsplus-agent/config.js`)
+    paths.push(`/etc/${appname}/config.js`)
+  }
+  if(process.platform == 'win32'){
+    let dir = path.join(process.env.APPDATA,appname)
+    try{ fs.mkdirSync(dir) }catch(e){}
+    fs.writeFileSync(path.join(dir,'config.js'),fs.readFileSync(path.join(__dirname,'config.js.sample')))
+    paths.push(path.join(dir,'config.js'))
   }
   create = ''
   return { paths, create }
 }
-
 
 function loadConfig(){
   let {paths} = getConfigPaths()
