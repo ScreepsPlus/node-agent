3e9fdd8cc26fa70d3cf5a22c9efcbeb58b8de62e
Adam Shumann <ashumann13@gmail.com>
2017-12-29 08:34:20 -0600

    Update for tokens

--- app.js
+++ app.js
@@ -1,52 +1,57 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
-const updateNotifier = require('update-notifier');
+const fs = require('fs')
+const path = require('path')
+const updateNotifier = require('update-notifier')
 const { ScreepsAPI } = require('screeps-api')
 const request = require('request')
 const editor = require('editor')
-const pkg = require('./package.json');
+const pkg = require('./package.json')
 let api
 let setupRan = false
 
-if(process.argv[2] == 'test') process.exit(0) // Placeholder ;)
+if (process.argv[2] == 'test') process.exit(0) // Placeholder ;)
 
-let {file,config} = loadConfig()
-if(config)
-  start()
-else
-  setup()
+let {file, config} = loadConfig()
+if (config) { start() } else { setup() }
 
-
-function start(){
-  if(config.sampleConfig || !config.screeps || !config.service){
+function start () {
+  if (config.sampleConfig || !config.screeps || !config.service) {
     console.log(file, 'does not have a valid config')
     return setup()
   }
-  if(config.checkForUpdates)
-    updateNotifier({pkg}).notify()
+  if (config.checkForUpdates) { updateNotifier({pkg}).notify() }
   api = new ScreepsAPI(config.screeps.connect)
-  api.auth(config.screeps.username, config.screeps.password).then((res)=>{
-    console.log('Authenticated')
-    console.log('Using stats method',config.screeps.method)
-    if(config.screeps.method == 'console')
-      beginConsoleStats()
-    else
-      beginMemoryStats()
-  })
+  if (config.screeps.username) {
+    console.log(`Update your config (${file}) to use auth tokens instead of username. http://blog.screeps.com/2017/12/auth-tokens/`)
+    console.log(`ex: {`)
+    console.log(`       token: "yourToken"`)
+    console.log(`    }`)
+    process.exit()
+  }
+  api.token = config.screeps.token
+  // api.auth(config.screeps.username, config.screeps.password).then((res) => {
+  // console.log('Authenticated')
+  console.log('Using stats method', config.screeps.method)
+  if (config.screeps.method === 'console') {
+    beginConsoleStats()
+  } else {
+    beginMemoryStats()
+  }
+  // })
 }
 
-function beginConsoleStats(){
+function beginConsoleStats () {
   api.socket.connect()
-  api.socket.on('connected',()=>{
-    api.socket.subscribe('console')    
+  api.socket.on('connected', () => {
+    api.socket.subscribe('console')
   })
-  api.socket.on('console',(event)=>{
+  api.socket.on('console', (event) => {
     console.log(event)
-    if(event.data.messages && event.data.messages.log)
+    if (event.data.messages && event.data.messages.log) {
       event.data.messages.log
-        .filter(l=>l.startsWith('STATS'))
-        .forEach(log=>processStats(log.slice(6).replace(/;/g,'\n')))
+        .filter(l => l.startsWith('STATS'))
+        .forEach(log => processStats(log.slice(6).replace(/;/g, '\n')))
+    }
   })
 }
 
@@ -66,53 +71,51 @@ function formatStats (data) {
   return Promise.resolve({ type, tick, time, stats })
 }
 
-function beginMemoryStats(){
+function beginMemoryStats () {
   tick()
-  setInterval(tick,15000)
+  setInterval(tick, 15000)
 }
-function addProfileData(stats){
-    return api.me().then(res=>{
-      let credits = res.money || 0
-      let power = res.power || 0
-      if(stats.type == 'application/json'){
-        stats.stats.credits = credits
-        stats.stats.power = power
-      }
-      if(stats.type == 'text/grafana'){
-        stats.stats += `credits ${credits} ${Date.now()}\n`
-        stats.stats += `power ${power} ${Date.now()}\n`
-      }
-      if(stats.type == 'text/influxdb')
-        stats.stats += `profile,user=${api.user.username} credits=${credits},power=${power} ${Date.now()}\n`
-      return stats
-    });
+function addProfileData (stats) {
+  return api.me().then(res => {
+    let credits = res.money || 0
+    let power = res.power || 0
+    if (stats.type == 'application/json') {
+      stats.stats.credits = credits
+      stats.stats.power = power
+    }
+    if (stats.type == 'text/grafana') {
+      stats.stats += `credits ${credits} ${Date.now()}\n`
+      stats.stats += `power ${power} ${Date.now()}\n`
+    }
+    if (stats.type == 'text/influxdb') { stats.stats += `profile,user=${api.user.username} credits=${credits},power=${power} ${Date.now()}\n` }
+    return stats
+  })
 }
-function addLeaderboardData(stats){
-    let leaderboardUrl = `/api/leaderboard/find?mode=world&username=${api.user.username}`;
-    return api.leaderboard.find(api.user.username,'world').then(res=>{
-      let { rank, score } = res.list.slice(-1)[0];
-      if(stats.type == 'application/json'){
-        stats.stats.leaderboard = { rank, score }
-      }
-      if(stats.type == 'text/grafana'){
-        stats.stats += `leaderboard.rank ${rank} ${Date.now()}\n`
-        stats.stats += `leaderboard.score ${score} ${Date.now()}\n`
-      }
-      if(stats.type == 'text/influxdb')
-        stats.stats += `leaderboard,user=${api.user.username} rank=${rank},score=${score} ${Date.now()}\n`
-      return stats
-    });
-}
- 
-function tick(){
-  Promise.resolve()
-    .then(()=>console.log('Fetching Stats'))
-    .then(getStats)
-    .then(processStats)
-    .catch(err=>console.error(err))
+function addLeaderboardData (stats) {
+  let leaderboardUrl = `/api/leaderboard/find?mode=world&username=${api.user.username}`
+  return api.leaderboard.find(api.user.username, 'world').then(res => {
+    let { rank, score } = res.list.slice(-1)[0]
+    if (stats.type == 'application/json') {
+      stats.stats.leaderboard = { rank, score }
+    }
+    if (stats.type == 'text/grafana') {
+      stats.stats += `leaderboard.rank ${rank} ${Date.now()}\n`
+      stats.stats += `leaderboard.score ${score} ${Date.now()}\n`
+    }
+    if (stats.type == 'text/influxdb') { stats.stats += `leaderboard,user=${api.user.username} rank=${rank},score=${score} ${Date.now()}\n` }
+    return stats
+  })
 }
 
-function processStats(data){
+function tick () {
+  Promise.resolve()
+    .then(() => console.log('Fetching Stats'))
+    .then(getStats)
+    .then(processStats)
+    .catch(err => console.error(err))
+}
+
+function processStats (data) {
   return Promise.resolve(data)
     .then(formatStats)
     .then(addProfileData)
@@ -120,21 +123,21 @@ function processStats(data){
     .then(pushStats)
 }
 
-function getStats(){
-    if(config.screeps.segment) {
-        return api.memory.segment.get(config.screeps.segment, config.screeps.shard || 'shard0').then(r => r.data)
-    } else {
-        return api.memory.get('stats', config.screeps.shard || 'shard0').then(r => r.data)
-    }
+function getStats () {
+  if (config.screeps.segment) {
+    return api.memory.segment.get(config.screeps.segment, config.screeps.shard || 'shard0').then(r => r.data)
+  } else {
+    return api.memory.get('stats', config.screeps.shard || 'shard0').then(r => r.data)
+  }
 }
 
-function pushStats(data){
-  let {type,stats} = data
-  if(!stats) return console.log('No stats found, is Memory.stats defined?')
-  if(config.showRawStats) console.log('Stats:',JSON.stringify(stats,null,3))
+function pushStats (data) {
+  let {type, stats} = data
+  if (!stats) return console.log('No stats found, is Memory.stats defined?')
+  if (config.showRawStats) console.log('Stats:', JSON.stringify(stats, null, 3))
   console.log('Pushing stats')
   let sconfig = config.service
-  if(type == 'application/json') stats = JSON.stringify(stats)
+  if (type == 'application/json') stats = JSON.stringify(stats)
   request({
     method: 'POST',
     url: sconfig.url + '/api/stats/submit',
@@ -142,77 +145,76 @@ function pushStats(data){
       user: 'token',
       pass: sconfig.token
     },
-    headers:{
-      'content-type':type
+    headers: {
+      'content-type': type
     },
     body: stats
-  },(err,res,data)=>{
-    if(res && res.statusCode == 413){
-      let len = Math.round(JSON.stringify(stats).length/1024)
+  }, (err, res, data) => {
+    if (res && res.statusCode == 413) {
+      let len = Math.round(JSON.stringify(stats).length / 1024)
       console.log(`stats size: ${len}kb`)
       console.log(`stats limit: 10mb (As of Mar 28, 2017) (If you hit this limit, you are probably doing something wrong)`)
       console.error(`It appears your stats data is too large, please check to make sure you are not submitting unneeded stats, such as old rooms. \n If you legitimately need to submit stats this large, contact ags131 on slack for a limit bump`)
     }
-    console.log('Result:',data)
-    if(err) console.error(err)
+    console.log('Result:', data)
+    if (err) console.error(err)
   })
 }
 
-function setup(){
-  if(setupRan){
+function setup () {
+  if (setupRan) {
     console.log('Agent not configured. Did you forget to edit the config?')
     process.exit()
   }
   setupRan = true
   let path = getConfigPaths().create
-  if(path){
-    fs.writeFileSync(path,fs.readFileSync(__dirname + '/config.js.sample'))
-    editor(path,(code)=>{
-      if(!code) start()
+  if (path) {
+    fs.writeFileSync(path, fs.readFileSync(__dirname + '/config.js.sample'))
+    editor(path, (code) => {
+      if (!code) start()
     })
-  }else{
+  } else {
     console.log('Please setup config.js before running.')
     console.log(`Valid paths for your platform (${process.platform}):`)
-    getConfigPaths().paths.forEach(path=>console.log(`- ${path}`))
+    getConfigPaths().paths.forEach(path => console.log(`- ${path}`))
     console.log()
     console.log('Or set the AGENT_CONFIG_PATH environment variable to point to a valid config file.')
   }
 }
 
-function getConfigPaths(){
+function getConfigPaths () {
   let appname = 'screepsplus-agent'
   let paths = []
-  if(process.env.AGENT_CONFIG_PATH)
-    paths.push(process.env.AGENT_CONFIG_PATH)
-  paths.push(path.join(__dirname,'config.js'))
+  if (process.env.AGENT_CONFIG_PATH) { paths.push(process.env.AGENT_CONFIG_PATH) }
+  paths.push(path.join(__dirname, 'config.js'))
   let create = ''
-  if(process.platform == 'linux' || process.platform == 'darwin'){
+  if (process.platform == 'linux' || process.platform == 'darwin') {
     create = `${process.env.HOME}/.${appname}`
     paths.push(create)
     paths.push(`/etc/${appname}/config.js`)
   }
-  if(process.platform == 'win32'){
-    let dir = path.join(process.env.APPDATA,appname)
-    try{ fs.mkdirSync(dir) }catch(e){}
-    if(!fs.existsSync(path.join(dir,'config.js'))){
-      fs.writeFileSync(path.join(dir,'config.js'),fs.readFileSync(path.join(__dirname,'config.js.sample')))
+  if (process.platform == 'win32') {
+    let dir = path.join(process.env.APPDATA, appname)
+    try { fs.mkdirSync(dir) } catch (e) {}
+    if (!fs.existsSync(path.join(dir, 'config.js'))) {
+      fs.writeFileSync(path.join(dir, 'config.js'), fs.readFileSync(path.join(__dirname, 'config.js.sample')))
     }
-    paths.push(path.join(dir,'config.js'))
+    paths.push(path.join(dir, 'config.js'))
   }
   create = ''
   return { paths, create }
 }
 
-function loadConfig(){
+function loadConfig () {
   let {paths} = getConfigPaths()
-  for(let i in paths){
+  for (let i in paths) {
     let file = paths[i]
-    try{
-      console.log('Try',file)
+    try {
+      console.log('Try', file)
       let config = require(file)
       console.log(file)
       return { config, file }
-    }catch(e){}
+    } catch (e) {}
   }
   return false
 }
